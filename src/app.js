aaf77b5d299154a6a99769847e9050e9345e5436
Erik <erik.silva@maps.com.br>
2022-03-30 10:26:48 -0300

    criaçao de rotas update e get id

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json()) // recurso pra interpretar json nas requisições
+
 const livros = [
     {id:1, "titulo": "Senhor dos aneis"},
     {id:2, "titulo": "O hobbit"}
@@ -18,6 +20,27 @@ app.get('/livros', (req,res) => {
     res.status(200).json(livros)
 })
 
+app.put('/livros/:id', (req,res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+    res.send('Livro cadastrado com sucesso');
+})
+
+app.post('/livros', (req,res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso')
+})
+
+app.put('/livros/:id', (req,res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+    res.send('Livro cadastrado com sucesso');
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id);
+}
 
 // exportar o arquivo pra fazer uso no server
 
