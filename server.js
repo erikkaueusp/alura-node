85a155b75f2766b42315c503ec5621a5bc82597c
Erik <erik.silva@maps.com.br>
2022-03-30 08:51:44 -0300

    alterando rotas e utilizando nodemon

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+// istalado nodemon 2.0.15 que faz o live server
+
 const http = require("http")  // importa o módulo nativo http.
 const port = 3000;  // porta que vai escutar a requisição
+
+const rotas = {
+    '/':'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores'
+}
 
 
 const server = http.createServer((req,res) => {
     // status OK e devolve o content-type
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Curso de Node'); 
+    res.end(rotas[req.url]); 
 
 }) // cria o servidor e escuta, precisa ter requisição e resposta e o callback
 
